d7e27525511247aaf5db36ece789f6ab94479db9
kimjaeseop <islandtim9463@gmail.com>
2023-06-02 12:45:33 +0900

    asd

--- javaStudy/src/kr/co/greenart/Constructor.java
+++ javaStudy/src/kr/co/greenart/Constructor.java
@@ -13,7 +13,7 @@ public class Constructor {
 		Product p2 = new Product("마우스", 25000, 5);
 		System.out.printf("%s , %d , %d \n", p2.name, p2.price, p2.count);
 		
-		Product p3 = new Product("키보드bbbb", 50000);
+		Product p3 = new Product("키ccccc보드cccccc", 50000);
 		System.out.printf("%s , %d , %d ", p3.name, p3.price, p3.count);
 		
 		Product p1 = new Product();
